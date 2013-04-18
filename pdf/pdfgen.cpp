7716f56b3cf0d479fc10d726e19a79f1bb3b6328
Michael Uman <Michael_Uman@sigmadesigns.com>
2013-04-17 19:19:14 -0700

    Moved page generation to unique functions.
    * Seperate functions for page1 and page2.

--- pdf/pdfgen.cpp
+++ pdf/pdfgen.cpp
@@ -1,35 +1,47 @@
 #include <cairo.h>
 #include <cairo-pdf.h>
 
-int main(void) 
-{
-  cairo_surface_t *surface;
-  cairo_t *cr;
+void gen_page_1(cairo_t* cr) {
+	cairo_move_to(cr, 10.0, 50.0);
+	cairo_show_text(cr, "Michael A. Uman");
 
-  surface = cairo_pdf_surface_create("pdffile.pdf", 612, 792);
-  cr = cairo_create(surface);
+	cairo_set_line_width(cr, 1.0);
 
-  cairo_set_source_rgb(cr, 0, 0, 0);
-  cairo_select_font_face (cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
-      CAIRO_FONT_WEIGHT_NORMAL);
-  cairo_set_font_size (cr, 40.0);
+	cairo_rectangle( cr, 20.0, 60.0, 430.0, 430.0);
+	cairo_stroke(cr);
 
-  cairo_move_to(cr, 10.0, 50.0);
-  cairo_show_text(cr, "Michael A. Uman");
+	cairo_show_page(cr);
 
-  cairo_set_line_width(cr, 1.0);
-  
-  cairo_rectangle( cr, 20.0, 60.0, 430.0, 430.0);
-  cairo_stroke(cr);
-  
-//  cairo_copy_page(cr);
-  cairo_show_page(cr);
-  cairo_move_to(cr, 10.0, 50.0);
-  cairo_show_text(cr, "Page #2");
-  cairo_show_page(cr);
-
-  cairo_surface_destroy(surface);
-  cairo_destroy(cr);
-
-  return 0;
+	return;
 }
+
+void gen_page_2(cairo_t* cr) {
+	cairo_move_to(cr, 10.0, 50.0);
+	cairo_show_text(cr, "Page #2");
+
+	cairo_show_page(cr);
+	
+	return;
+}
+
+int main(void) {
+	cairo_surface_t *surface;
+	cairo_t *cr;
+
+	surface = cairo_pdf_surface_create("pdffile.pdf", 612, 792);
+	cr = cairo_create(surface);
+
+	cairo_set_source_rgb(cr, 0, 0, 0);
+	cairo_select_font_face (cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
+	  CAIRO_FONT_WEIGHT_NORMAL);
+	cairo_set_font_size (cr, 40.0);
+
+	gen_page_1(cr);
+	gen_page_2(cr);
+
+	cairo_surface_destroy(surface);
+	cairo_destroy(cr);
+
+	return 0;
+}
+
