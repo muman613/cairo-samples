ffbd6bff996f3bd700c9a8c646255befc9e9f9d1
Michael Uman <Michael_Uman@sigmadesigns.com>
2013-04-19 17:40:46 -0700

    Added pango-viewer to examples.
    * Copied pango-viewer from the pango source repository.
    * Modified to work with sample build system.

---
+++ pango-2/config.h
@@ -0,0 +1,15 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define PACKAGE_NAME 	"pango"
+#define PACKAGE_VERSION "1.28.0"
+#define MODULE_VERSION  "1.6.0"
+
+#define HAVE_CAIRO		1
+#define HAVE_XFT		1
+#define HAVE_FREETYPE	1
+#define HAVE_X			1
+#define HAVE_CAIRO_XLIB 1
+
+#endif
+
